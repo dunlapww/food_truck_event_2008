1839a6778cdadcee8390d3153578d598cc2c7235
Will Dunlap <dunlapww@gmail.com>
2020-09-15 13:10:42 -0600

    Pass it knows details

--- test/item_test.rb
+++ test/item_test.rb
@@ -11,7 +11,7 @@ class ItemTest < Minitest::Test
 
   def test_it_has_attributes
     item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-    assert_equal "Apple Pie(Slice)", item2.name
+    assert_equal "Apple Pie (Slice)", item2.name
     assert_equal 2.50, item2.price
   end
 
