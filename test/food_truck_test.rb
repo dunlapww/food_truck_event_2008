57488df05e19886ef516794b70f185512dfbb885
Will Dunlap <dunlapww@gmail.com>
2020-09-15 13:14:41 -0600

    Add test foodtruck has attributes

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -8,4 +8,11 @@ class FoodTruckTest < Minitest::Test
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     assert_instance_of FoodTruck, food_truck
   end
+
+  def test_it_has_attributes
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    assert_equal "Rocky Mountain Pies", food_truck.name
+    expected = {}
+    assert_equal expected, food_truck.inventory
+  end
 end
